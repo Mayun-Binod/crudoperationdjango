9bb5cef54344b7f817d7dd0662565eb27dfd0b76
Mayun-binod <shresthabinod334@gmail.com>
2024-10-02 02:02:38 +0545

    added

--- crudproject1/enroll/models.py
+++ crudproject1/enroll/models.py
@@ -5,3 +5,6 @@ class User(models.Model):
     name=models.CharField(max_length=100)
     email=models.EmailField(max_length=100)
     password=models.CharField(max_length=100)
+
+    def __str__(self):
+        return f'{self.name} ({self.email})'
--- crudproject1/enroll/views.py
+++ crudproject1/enroll/views.py
@@ -1,6 +1,22 @@
 from django.shortcuts import render
-
+from .forms import StudentRegistration
+from .models import User
 # Create your views here.
 
 def add_show(request):
-    return render(request,'enroll/addandshow.html')
+    if request.method == 'POST':
+        fm=StudentRegistration(request.POST)
+        if fm.is_valid():
+            # fm.save() one method
+            # another method
+            nm=fm.cleaned_data['name']
+            em=fm.cleaned_data['email']
+            pw=fm.cleaned_data['password']
+            res=User(name=nm,email=em,password=pw)
+            # res=User(name=nm,email=em) # if you don't want to show in databases of password
+            res.save()
+            fm=StudentRegistration() # to blank just like reset the information of forms
+
+    else:
+        fm=StudentRegistration()
+    return render(request,'enroll/addandshow.html',{'form':fm})
